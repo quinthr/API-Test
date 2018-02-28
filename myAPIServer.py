70c261908b3f438a6a367ba1930e0360294556a6
Quinth Anthony A. Razuman <31249145+quinthr@users.noreply.github.com>
2018-03-01 06:47:52 +0800

    Second Assignment

---
+++ myAPIServer.py
@@ -0,0 +1,127 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'imbatman'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\Gigabyte\\Desktop\\myAPIServer\\user.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    email = db.Column(db.String(50), unique=True)
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(id=data['id']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+@app.route('/user', methods=['GET'])
+@token_required
+def get_users(current_user):
+
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['email'] = user.email
+        output.append(user_data)
+
+    return jsonify({'users' : output})
+
+@app.route('/user/<id>', methods=['GET'])
+@token_required
+def get_user(current_user, id):
+
+    user = User.query.filter_by(id=id).first()
+
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+
+    user_data = {}
+    user_data['id'] = user.id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['email'] = user.email
+
+    return jsonify({'user' : user_data})
+
+@app.route('/user', methods=['POST'])
+def create_user():
+
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(name=data['name'], password=hashed_password, email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message' : 'New user created!'})
+
+@app.route('/user/<id>', methods=['DELETE'])
+@token_required
+def delete_user(current_user, id):
+
+    user = User.query.filter_by(id=id).first()
+
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message' : 'The user has been deleted!'})
+
+@app.route('/login')
+def login():
+
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Not Authenticated')
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('User Not Found')
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Invalid Password')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
